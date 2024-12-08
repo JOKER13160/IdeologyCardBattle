a94c627ed2a97da21a70a4198399812af40738ce
ToyamaHiroto <kuromaru147@gmail.com>
2024-12-08 12:21:55 +0900

    競合スクリプトを削除 仕切り直し

--- Assets/Scripts/Cards/CradDataSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Create CardDataSO", fileName = "CardDataSO")]
-public class CardDataSO : ScriptableObject
-{
-    public List<CardData> cardDatasList = new();
-}
--- Assets/Scripts/Decks/DeckDataSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Create DeckDataSO", fileName = "DeckDataSO")]
-public class DeckDataSO : ScriptableObject
-{
-    public List<DeckData> deckDatasList = new();
-}
